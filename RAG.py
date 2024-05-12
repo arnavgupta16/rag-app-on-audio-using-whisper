39039a903d2d6b881eba6c2017ce796b31550024
arnavgupta16 <113133432+arnavgupta16@users.noreply.github.com>
2024-05-12 14:58:32 +0530

    Update RAG.py

--- RAG.py
+++ RAG.py
@@ -11,7 +11,7 @@ import openai
 import os
 import speech_recognition as sr
 load_dotenv()
-openai.api_key = "sk-proj-he3booPfQSsgfDOKD8CWT3BlbkFJifzb1fI7SY22HDFuiuev"
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def get_audio_text(audio_files):
     r = sr.Recognizer()
